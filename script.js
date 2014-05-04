a841c359854383b7c75c909199fedfcdcd0802b5
Wayra <pierostm2000@hotmail.com>
2014-05-03 21:59:14 -0500

    g

---
+++ script.js
@@ -0,0 +1,15 @@
+var $button = $('#mas');
+ 
+ $.get('usuario.json',function(info){
+ 	var creaciones = info.creaciones;
+
+ 	if (creaciones == 0){
+ 		$('section').prepend('<h2>No tienes creaciones <br>Inicia un proyecto</h2>');
+ 	}
+ 	else{
+ 		
+ 		$button.remove();
+ 	}
+ 		
+
+ })
